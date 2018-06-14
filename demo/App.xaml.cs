e9a44bfe7f3b2cf7c516633b6ff783ccc56c1582
Macbook <macbook@Macbooks-MacBook-Pro.local>
2018-06-14 18:59:42 +0530

    Projects created

---
+++ demo/App.xaml.cs
@@ -0,0 +1,35 @@
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace demo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("ios=0dd6932c-a1f2-4012-a9dd-b68a0ed20ec5;", typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
